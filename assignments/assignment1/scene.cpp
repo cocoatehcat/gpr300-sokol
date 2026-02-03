34f3a0597e30f822b82022b1b1abc57b459d56e4
cocoatehcat <cocoa.teh.cat@gmail.com>
2026-02-02 19:29:14 -0500

    feat: created assignment 1

---
+++ assignments/assignment1/scene.cpp
@@ -0,0 +1,191 @@
+#include "scene.h"
+
+// imgui
+#include "imgui/imgui.h"
+#include "imguizmo/ImGuizmo.h"
+
+#include <glm/gtc/type_ptr.hpp>
+
+// batteries
+#include "batteries/opengl.h"
+#include "batteries/lights.h"
+
+//#include "cocoa/materialsReader.h"
+
+struct {
+    float alpha = 1.0f;
+    glm::vec3 ambient = {0.3, 0.3, 0.3};
+    int selectedIndex = 0;
+
+} debug;
+
+typedef struct // making new type of struct instead of unordered map
+{
+    const std::string name;
+    const batteries::material_t material;
+} mats;
+
+static std::vector<mats> matList = {
+    {"default", {{0.5f, 0.5f, 0.5f}, {0.5f, 0.5f, 0.5f}, {0.5f, 0.5f, 0.5f}, 1.0f}},
+
+    // http://devernay.free.fr/cours/opengl/materials.html
+    // Since my file reader imploded we will just do this the hideous way
+    {"emerald", {{0.0215f, 0.1745f, 0.0215f}, {0.07568f, 0.61424f, 0.07568f}, {0.633f, 0.727811f, 0.633f}, 0.6f}},
+    {"jade", {{0.135f, 0.2225f, 0.1575f}, {0.54f, 0.89f, 0.63f}, {0.316228f, 0.316228f, 0.316228f}, 0.1}},
+    {"obsidian", {{0.05375f, 0.05f, 0.06625f}, {0.18275f, 0.17f, 0.22525f}, {0.332741f, 0.328634f, 0.346435f}, 0.3f}},
+    {"pearl", {{0.25f, 0.20725f, 0.20725f}, {1.0f, 0.829f, 0.829f}, {0.296648f, 0.296648f, 0.296648f}, 0.088f}},
+    {"ruby", {{0.1745f, 0.01175f, 0.01175f}, {0.61424f, 0.04136f, 0.04136f}, {0.727811f, 0.626959f, 0.626959f}, 0.6f}},
+    {"turquoise", {{0.1f, 0.18725f, 0.1745f}, {0.396f, 0.74151f, 0.69102f}, {0.297254f, 0.30829f, 0.306678f}, 0.1f}},
+    {"brass", {{0.329412f, 0.223529f, 0.027451f}, {0.780392f, 0.568627f, 0.113725f}, {0.992157f, 0.941176f, 0.807843f}, 0.21794872f}},
+    {"bronze", {{0.2125f, 0.1275f, 0.054f}, {0.714f, 0.4284f, 0.18144f}, {0.393548f, 0.271906f, 0.166721f}, 0.2f}},
+    {"chrome", {{0.25f, 0.25f, 0.25f}, {0.4f, 0.4f, 0.4f}, {0.774597f, 0.774597f, 0.774597f}, 0.6f}},
+    {"copper", {{0.19125f, 0.0735f, 0.0225f}, {0.7038f, 0.27048f, 0.0828f}, {0.256777f, 0.137622f, 0.086014f}, 0.1f}},
+    {"gold", {{0.24725f, 0.1995f, 0.0745f}, {0.75164f, 0.60648f, 0.22648f}, {0.628281f, 0.555802f, 0.366065f}, 0.4f}},
+    {"silver", {{0.19225f, 0.19225f, 0.19225f}, {0.50754f, 0.50754f, 0.50754f}, {0.508273f, 0.508273f, 0.508273f}, 0.4f}},
+    {"black plastic", {{0.0f, 0.0f, 0.0f}, {0.01f, 0.01f, 0.01f}, {0.50f, 0.50f, 0.50f}, 0.25f}},
+    {"cyan plastic", {{0.0f, 0.1f, 0.06f}, {0.0f, 0.50980392f, 0.50980392f}, {0.50196078f, 0.50196078f, 0.50196078f}, 0.25f}},
+    {"green plastic", {{0.0f, 0.0f, 0.0f}, {0.1f, 0.35f, 0.1f}, {0.45f, 0.55f, 0.45f}, 0.25f}},
+    {"red plastic", {{0.0f, 0.0f, 0.0f}, {0.5f, 0.0f, 0.0f}, {0.7f, 0.6f, 0.6f}, 0.25f}},
+    {"white plastic", {{0.0f, 0.0f, 0.0f}, {0.55f, 0.55f, 0.55f}, {0.70f, 0.70f, 0.70f}, 0.25f}},
+    {"yellow plastic", {{0.0f, 0.0f, 0.0f}, {0.5f, 0.5f, 0.0f}, {0.60f, 0.60f, 0.50f}, 0.25f}},
+    {"black rubber", {{0.02f, 0.02f, 0.02f}, {0.01f, 0.01f, 0.01f}, {0.4f, 0.4f, 0.4f}, 0.078125f}},
+    {"cyan rubber", {{0.0f, 0.05f, 0.05f}, {0.4f, 0.5f, 0.5f}, {0.04f, 0.7f, 0.7f}, 0.078125f}},
+    {"green rubber", {{0.0f, 0.05f, 0.0f}, {0.4f, 0.5f, 0.4f}, {0.04f, 0.7f, 0.04f}, 0.078125f}},
+    {"red rubber", {{0.05f, 0.0f, 0.0f}, {0.5f, 0.4f, 0.4f}, {0.7f, 0.04f, 0.04f}, 0.078125f}},
+    {"white rubber", {{0.05f, 0.05f, 0.05f}, {0.5f, 0.5f, 0.5f}, {0.7f, 0.7f, 0.7f}, 0.078125f}},
+    {"yellow rubber", {{0.05f, 0.05f, 0.0f}, {0.5f, 0.5f, 0.4f}, {0.7f, 0.7f, 0.04f}, 0.078125f}},
+};
+
+
+Scene::Scene()
+{
+    suzanne = std::make_unique<ew::Model>("assets/models/suzanne.obj");
+    blinnphong = std::make_unique<ew::Shader>("assets/shaders/default.vs", "assets/shaders/blinnphong.fs");
+    leaves = std::make_unique<ew::Texture>("assets/textures/leaves2.jpeg");
+    ornament = std::make_unique<ew::Texture>("assets/textures/CTO_Color.jpg");
+    normalMap = std::make_unique<ew::Texture>("assets/textures/CTO_NormalGL.jpg");
+
+    light = {
+        .color = {1.0f, 0.0f, 1.0f},
+        .position = {2.0f, 2.0f, 2.0f},  
+    };
+
+}
+
+Scene::~Scene()
+{
+}
+
+void Scene::Update(float dt)
+{
+    batteries::Scene::Update(dt);
+
+    /* body */
+}
+
+void Scene::Render(void)
+{
+    const auto view_proj = camera.Projection() * camera.View();
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glEnable(GL_DEPTH_TEST);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, leaves->getID());
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, ornament->getID());
+
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, normalMap->getID());
+
+    blinnphong->use();
+
+    // scene matrices
+    blinnphong->setMat4("model", glm::mat4(1.0f));
+    blinnphong->setMat4("view_proj", view_proj);
+    blinnphong->setVec3("camera_position", camera.position);
+
+    // Set uniforms
+    blinnphong->setVec3("camera", camera.position);
+    blinnphong->setVec3("light.position", light.position);
+    blinnphong->setVec3("light.color", light.color);
+    blinnphong->setFloat("alpha", debug.alpha);
+    blinnphong->setVec3("ambientColor", debug.ambient);
+
+    // Updating uniforms for lighting
+    auto material = matList[debug.selectedIndex].material;
+    blinnphong->setVec3("material.ambient", material.ambient);
+    blinnphong->setVec3("material.diffuse", material.diffuse);
+    blinnphong->setVec3("material.specular", material.specular);
+    blinnphong->setFloat("material.shininess", material.shininess);
+
+    // Texture test
+    blinnphong->setInt("mainTexture", 1);
+    blinnphong->setInt("normalMap", 2);
+
+    // draw suzanne
+    suzanne->draw();
+}
+
+glm::mat4 identity(1.0f);
+
+void Scene::Debug(void)
+{
+    const auto view = camera.View();
+    const auto proj = camera.Projection();
+
+    ImGuizmo::BeginFrame();
+    ImGuizmo::SetDrawlist(ImGui::GetBackgroundDrawList());
+    ImGuizmo::SetRect(0, 0, ImGui::GetIO().DisplaySize.x, ImGui::GetIO().DisplaySize.y);
+
+    ImGuizmo::DrawGrid(&view[0][0], &proj[0][0], glm::value_ptr(identity), 10.0f);
+
+    auto light_matrix = glm::translate(glm::mat4(1.0f), light.position);
+    ImGuizmo::Manipulate(&view[0][0], &proj[0][0], ImGuizmo::OPERATION::TRANSLATE, ImGuizmo::MODE::WORLD, glm::value_ptr(light_matrix));
+
+    if (ImGuizmo::IsUsing()){
+        light.position = glm::vec3(light_matrix[3]);
+    }
+
+    cameracontroller.Debug();
+
+    ImGui::Begin("Controls", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+
+    ImGui::Checkbox("Paused", &time.paused);
+    ImGui::SliderFloat("Time Factor", &time.factor, 0.0f, 10.0f);
+
+    /* build debug ui here */
+    ImGui::SeparatorText("Ambient");
+    ImGui::SliderFloat("Intensity", &debug.alpha, 0.0f, 1.0f);
+    ImGui::ColorEdit3("Color", &light.color[0]);
+
+    ImGui::SeparatorText("Material");
+    if (ImGui::BeginCombo("Presets", matList[debug.selectedIndex].name.c_str())) {
+        for (int i = 0; i < matList.size(); i++) {
+            const bool isSelected = (matList[debug.selectedIndex].name == matList[i].name);
+            if (ImGui::Selectable(matList[i].name.c_str(), isSelected)) {
+                debug.selectedIndex = i;
+            }
+
+            // Set the initial focus when opening the combo
+            // (scrolling + keyboard navigation focus)
+            if (isSelected) {
+                ImGui::SetItemDefaultFocus();
+            }
+
+        }
+        ImGui::EndCombo();
+    }
+    auto material = matList[debug.selectedIndex].material; // Updating the values
+    ImGui::SliderFloat3("Ambient", &material.ambient[0], 0.0f, 1.0f);
+    ImGui::SliderFloat3("Diffuse", &material.diffuse[0], 0.0f, 1.0f);
+    ImGui::SliderFloat3("Specular", &material.specular[0], 0.0f, 1.0f);
+    ImGui::SliderFloat("Shininess", &material.shininess, 2.0f, 128.0f);
+
+    ImGui::End();
+}
