fd6c9fa165367ea19b5e0a0ac37002184329ff54
cocoatehcat <cocoa.teh.cat@gmail.com>
2026-02-24 13:57:06 -0500

    fix: chromatic aberration looks normal now

--- assets/shaders/postprocessing/chromatic.fs
+++ assets/shaders/postprocessing/chromatic.fs
@@ -9,21 +9,16 @@ in vec2 vs_texcoord;
 uniform sampler2D screen;  
 
 float distortion = 0.75;
-
-vec2 Pincushion(vec2 uv, float strength) {
-    vec2 st = uv - 0.5;
-    float uvA = atan(st.x, st.y);
-    float uvD = dot(st, st);
-    return 0.5 + vec2(sin(uvA), cos(uvA) * sqrt(uvD) * (1.0 - strength * uvD));
-}
+vec3 offset = vec3(0.01, 0.005, -0.005);
+const vec2 direction = vec2(1.0);
 
 void main()
 {
     vec3 color = texture(screen, vs_texcoord).rgb;
 
-    float rChannel = texture(screen, Pincushion(vs_texcoord, 0.3 * distortion)).r;
-    float gChannel = texture(screen, Pincushion(vs_texcoord, 0.15 * distortion)).g;
-    float bChannel = texture(screen, Pincushion(vs_texcoord, 0.075 * distortion)).b;
+    float rChannel = texture(screen, vs_texcoord + (direction * vec2(distortion) * 0.01)).r;
+    float gChannel = texture(screen, vs_texcoord + (direction * vec2(distortion) * 0.005)).g;
+    float bChannel = texture(screen, vs_texcoord + (direction * vec2(distortion) * -0.005)).b;
 
     vec3 finalColor = vec3(rChannel, gChannel, bChannel);
 
