6ec1a807ed37504165432a83e26c95817da70e04
cocoatehcat <cocoa.teh.cat@gmail.com>
2026-04-26 21:55:14 -0400

    style: vignette

--- assets/shaders/cocoa/vignette.fs
+++ assets/shaders/cocoa/vignette.fs
@@ -13,13 +13,15 @@ void main()
 {
     // Referencing Shadertoy : https://www.shadertoy.com/view/lsKSWR
     vec2 uv = vs_texcoord;
+    // Center UVs and correct aspect ratio
     uv *= 1.0 - uv.yx;
 
+    // Define vignette intensity from center of screen
     float vig = uv.x * uv.y * 16.0;
     vig = pow(vig, 0.25);
-    //vec3 color = texture(screen, vs_texcoord).rgb;
 
+    // Adding effect to background
     vec3 color = backcolor;
-    color *= vec3(vig); // Changing this is cool, adding effect
+    color *= vec3(vig); // Changing this is cool
     FragColor = vec4(color, 1.0);
 }
