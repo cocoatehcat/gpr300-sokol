8b412b48cbe9cf72dd437e7bc780a7311bf07afb
IkoMerkel <iko.merkel@mymail.champlain.edu>
2026-04-27 18:46:51 -0400

    Last push for fog

--- assignments/finalWithFog/scene.cpp
+++ assignments/finalWithFog/scene.cpp
@@ -27,10 +27,6 @@ struct {
 
     float fogHeight = 1.0;
     float fogRange = 0.5;
-    bool fogtoggle = false;
-    float fogR = 0.5;
-    float fogG = 0.5;
-    float fogB = 0.5;
 
 } debug;
 
@@ -158,36 +154,6 @@ void Scene::createHeightBuffer() {
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
-void Scene::createIsolationBuffer() {
-    glCreateFramebuffers(1, &isolation_fbo);
-
-    glBindFramebuffer(GL_FRAMEBUFFER, isolation_fbo);
-
-    { // Create Depth
-
-        glGenTextures(1, &isolation_texture);
-        glBindTexture(GL_TEXTURE_2D,isolation_texture);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 800, 600, 0, GL_RGBA,GL_UNSIGNED_BYTE,NULL);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glFramebufferTexture2D(GL_FRAMEBUFFER,GL_COLOR_ATTACHMENT0,GL_TEXTURE_2D,isolation_texture,0);
-
-        glGenTextures(1, &isolation_depth);
-        glBindTexture(GL_TEXTURE_2D, isolation_depth);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, 800, 600, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, isolation_depth, 0);
-
-    }
-
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER != GL_FRAMEBUFFER_COMPLETE)) {
-        printf("It's not complete for height :(\n");
-    }
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-}
-
-
 void Scene::createFrameBuffer() {
     glCreateFramebuffers(1, &fbo);
 
@@ -220,6 +186,47 @@ void Scene::createFrameBuffer() {
     // All functions will be operating on Framebuffer
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
+
+struct Framebuffer {
+
+    GLuint framefbo; // frame buffer object
+    GLuint framefbo_texture;
+    GLuint framefbo_depth;
+    
+    void init() {
+        glCreateFramebuffers(1, &framefbo);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, framefbo);
+
+        { // Create Texture
+            glGenTextures(1, &framefbo_texture);
+            glBindTexture(GL_TEXTURE_2D, framefbo_texture);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, 800, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framefbo_texture, 0);
+
+            glGenTextures(1, &framefbo_depth);
+            glBindTexture(GL_TEXTURE_2D, framefbo_depth);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, 800, 600, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, framefbo_depth, 0);
+
+            // clean up
+            glBindTexture(GL_TEXTURE_2D, 0);
+        }
+        // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fbo_texture, 0);
+
+        if (glCheckFramebufferStatus(GL_FRAMEBUFFER != GL_FRAMEBUFFER_COMPLETE)) {
+            printf("It's not complete :(\n");
+        }
+
+        // Has to unbind or else it will create a black screen
+        // All functions will be operating on Framebuffer
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    }
+} framebuff;
     
 struct fullscreenQuad
 {
@@ -260,17 +267,28 @@ struct fullscreenQuad
 // Assigns effect, currently is the Vignette
 void Scene::assignEffect(ew::Shader* shader) {
 
-    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    //glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+    shader->use();
+    shader->setVec3("backcolor", debug.backgroundColor);
+    shader->setInt("sceneTexture", 0);
+    shader->setInt("sceneTexture", fbo_depth);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, elevation_texture);
+    shader->setInt("mist_effect", 1);
     glDisable(GL_DEPTH_TEST);
 
     //glClearColor(0.2f, 0.3f, 0.3f, 1.0f); Does nothing
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    shader->use();
-    shader->setVec3("backcolor", debug.backgroundColor);
-
     // no fullscreen
+
     glBindVertexArray(fullQuad.vao);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, framebuff.framefbo_texture);
     glDrawArrays(GL_TRIANGLES, 0, 6);
     
 }
@@ -280,11 +298,10 @@ Scene::Scene()
     monument = std::make_unique<ew::Model>("assets/models/MonumentValley-compressed.obj");
     ambient = std::make_unique<ew::Shader>("assets/shaders/cocoa/ambient.vs", "assets/shaders/cocoa/ambient.fs");
     elevation = std::make_unique<ew::Shader>("assets/shaders/IkoFinalShaders/elevation.vs", "assets/shaders/IkoFinalShaders/elevation.fs");
-    elevationFade = std::make_unique<ew::Shader>("assets/shaders/fullscreen.vs", "assets/shaders/IkoFinalShaders/mistfade.fs");
     depth = std::make_unique<ew::Shader>("assets/shaders/depth.vs", "assets/shaders/depth.fs");
 
     // Vignette! This can be changed
-    postProcessingEffects.push_back(std::make_unique<ew::Shader>("assets/shaders/cocoa/vignette.vs", "assets/shaders/cocoa/vignette.fs"));
+    postProcessingEffects.push_back(std::make_unique<ew::Shader>("assets/shaders/cocoa/vignette.vs", "assets/shaders/cocoa/vignetteInFront.fs"));
 
     light = {
         .color = {1.0f, 1.0f, 1.0f},
@@ -292,10 +309,12 @@ Scene::Scene()
     };
 
     fullQuad.Init();
+    framebuff.init();
 
-    createFrameBuffer();
+
     createHeightBuffer();
-    createIsolationBuffer();
+    // DONT USE MATTY THING
+    //createFrameBuffer();
 }
 
 Scene::~Scene()
@@ -313,11 +332,6 @@ void Scene::Update(float dt)
 
 void Scene::Render(void)
 {
-    
-    // Vignette, I'm too lazy to change from the previous system
-    // But let me know if it's a problem and I'll fix it
-    assignEffect(postProcessingEffects[debug.indexEffect].get());
-
     // Actual Beginning of Pipeline
     auto newModelMatrix = 
         glm::translate(glm::mat4(1.0f), debug.modelPos)
@@ -330,13 +344,17 @@ void Scene::Render(void)
     // Model Pipeline
     const auto view_proj = camera.Projection() * camera.View();
 
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuff.framefbo);
+
     // Will Clear Vignette
-    //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(debug.backgroundColor.x, debug.backgroundColor.y, debug.backgroundColor.z, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_STENCIL_TEST); // Enable stencil
+    glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE); // Replace stencil value on pass
 
     // See through vignette
     glEnable(GL_BLEND);
@@ -364,20 +382,21 @@ void Scene::Render(void)
     // Scaling down the giant model!
     auto scale_matrix = glm::scale(glm::mat4(1.0f), glm::vec3(0.01f, 0.01f, 0.01f)); 
     ambient->setMat4("model", newModelMatrix);
+
+    glStencilFunc(GL_ALWAYS, 1, 0xFF); // Set stencil value to 1
+    glStencilMask(0xFF); // writing to stencil
+
     monument->draw();
+
+    glStencilFunc(GL_NOTEQUAL, 1, 0xFF); // Set stencil value to 1
+    glStencilMask(0x00);
 
     // Cleaning up, just in case
     glDisable(GL_BLEND);
 
-    // Blitzing the vignette with the model
-    glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo);
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-    glBlitFramebuffer(0, 0, kFramebufferWidth, kFramebufferHeight, 0, 0, kFramebufferWidth, kFramebufferHeight, GL_COLOR_BUFFER_BIT, GL_NEAREST);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-    if(debug.fogtoggle)
-    {
-        glBindFramebuffer(GL_FRAMEBUFFER, elevation_fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, elevation_fbo);
     {
         const auto view_proj = camera.Projection() * camera.View();
 
@@ -425,37 +444,19 @@ void Scene::Render(void)
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 
-    glBindFramebuffer(GL_FRAMEBUFFER, isolation_texture);
-    {
-        glClearColor(debug.backgroundColor.x ,debug.backgroundColor.y, debug.backgroundColor.z, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  
-        glBindVertexArray(fullQuad.vao);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, fbo_texture);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, elevation_texture);
-        glActiveTexture(GL_TEXTURE2);
-        glBindTexture(GL_TEXTURE_2D, fbo_texture);
-        elevationFade->use();
-        elevationFade->setInt("screen", 0);
-        elevationFade->setInt("mist_effect", 1);
-        elevationFade->setInt("background", 2);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-    }
 
 
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-    }
-    
 
-    //glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
-    //glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fullQuad.vao);
+
+    // Vignette, I'm too lazy to change from the previous system
+    // But let me know if it's a problem and I'll fix it
+    assignEffect(postProcessingEffects[debug.indexEffect].get());
+
+    // Blitzing the vignette with the model
+    //glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo);
+    //glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
     //glBlitFramebuffer(0, 0, kFramebufferWidth, kFramebufferHeight, 0, 0, kFramebufferWidth, kFramebufferHeight, GL_COLOR_BUFFER_BIT, GL_NEAREST);
     //glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-
 }
 
 void Scene::Debug(void)
@@ -530,28 +531,24 @@ void Scene::Debug(void)
         debug.palette1 = lerp(dayPalette.accent1, nightPalette.accent1, debug.lerpFactor);
         debug.palette2 = lerp(dayPalette.accent2, nightPalette.accent2, debug.lerpFactor);
     }
+
     if (ImGui::CollapsingHeader("Fog"))
     {
         ImVec2 uv_min(0.0f, 1.0f);
         ImVec2 uv_max(1.0f, 0.0f);
 
-        debug.fogtoggle = true;
-        ImGui::SliderFloat("Fog Height", &debug.fogHeight, -15.0f, 8.0f);
+        ImGui::SliderFloat("Fog Height", &debug.fogHeight, -5.0f, 5.0f);
         ImGui::SliderFloat("Fog Fade Range", &debug.fogRange, 0.1f, 3.0f);
 
         ImGui::Text("Elevation:");
         ImGui::Image((ImTextureID)(intptr_t) elevation_texture, ImVec2(200, 150), uv_min, uv_max);
-
-        //ImGui::Text("Isolation:");
-        //ImGui::Image((ImTextureID)(intptr_t) isolation_texture, ImVec2(200, 150), uv_min, uv_max);
-    }
-    else{
-        debug.fogtoggle = false;
     }
     // Anything else that's cute?
     
     if (ImGui::CollapsingHeader("Framebuffer Images")) {
-        ImGui::Image((void*)(intptr_t)fbo_texture, ImVec2(400, 300), ImVec2(0, 1), ImVec2(1, 0));
+        //ImGui::Image((void*)(intptr_t)fbo_texture, ImVec2(400, 300), ImVec2(0, 1), ImVec2(1, 0));
+
+        ImGui::Image((void*)(intptr_t)framebuff.framefbo_texture, ImVec2(400, 300), ImVec2(0, 1), ImVec2(1, 0));
     }
 
     ImGui::End();
